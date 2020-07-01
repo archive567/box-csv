596a1aaad367db6f1fdd27b4ca37e984450d9526
Tony Day <tonyday567@gmail.com>
2020-07-01 13:11:43 +1000

    tests added, test csv changed to covid

--- src/Data/Csv.hs
+++ src/Data/Csv.hs
@@ -16,13 +16,21 @@ module Data.Csv
     rowEmitter,
     rowCommitter,
     runCsv,
+
+    -- * parsers
     sep,
+    field_,
     field,
+    skipField_,
     skipField,
-    texts,
+    int,
+    int',
+    A.double,
+    double',
+    fields,
     scis,
-    doubles,
     ints,
+    doubles,
   )
 where
 
@@ -34,32 +42,44 @@ import Data.Scientific
 import qualified Data.Text as Text
 import NumHask.Prelude
 
+-- $setup
+-- >>> :set -XOverloadedStrings
+
+-- | csv file configuration
 data CsvConfig
   = CsvConfig
-      { -- | data set name
+      { -- | file name stem
         name :: Text,
         -- | file suffix
         suffix :: Text,
-        -- | local directory
+        -- | directory
         dir :: Text,
-        -- | separator
-        csep :: Char,
-        -- | first row is a header row
+        -- | field separator
+        fsep :: Char,
+        -- | nature of header row(s)
         header :: Header
       }
   deriving (Show, Generic, Eq)
 
--- | CsvConfig "Hill_Valley_with_noise" ".csv" "./other" '\t' HasHeader
+-- | default csv file details
+--
+-- >>> defaultCsvConfig
+-- CsvConfig {name = "time_series_covid19_deaths_global_narrow", suffix = ".csv", dir = "./other", fsep = ',', header = HasHXL}
+--
+-- test data from https://data.humdata.org/dataset/novel-coronavirus-2019-ncov-cases
 defaultCsvConfig :: CsvConfig
 defaultCsvConfig =
   CsvConfig
-    "Hill_Valley_with_noise"
+    "time_series_covid19_deaths_global_narrow"
     ".csv"
     "./other"
-    '\t'
-    HasHeader
+    ','
+    HasHXL
 
--- | filepath for teh config.
+-- | filepath for the config.
+--
+-- >>> file defaultCsvConfig
+-- "./other/time_series_covid19_deaths_global_narrow.csv"
 file :: CsvConfig -> FilePath
 file cfg =
   cfg ^. #dir
@@ -68,89 +88,131 @@ file cfg =
     <> cfg ^. #suffix
       & unpack
 
--- | does the csv have a header row?
-data Header = HasHeader | NoHeader deriving (Show, Eq)
+-- | Type of header rows.  Note the modern propensity for multiple header rows.
+data Header = HasHeader | HasHXL | NoHeader deriving (Show, Eq)
 
--- | emits parsed csv rows
+-- | A continuation emitter of parsed csv rows from a CsvConfig, returning the original text on failure
+-- >>> rowEmitter defaultCsvConfig fields `with` emit
+-- Just (Right ["Province/State","Country/Region","Lat","Long","Date","Value","ISO 3166-1 Alpha 3-Codes","Region Code","Sub-region Code","Intermediate Region Code\r"])
 rowEmitter :: CsvConfig -> (Char -> A.Parser a) -> Cont IO (Emitter IO (Either Text a))
-rowEmitter cfg p = eParse (p (view #csep cfg)) <$> fileEmitter (file cfg)
+rowEmitter cfg p = eParse (p (view #fsep cfg)) <$> fileEmitter (file cfg)
 
--- | emits parsed csv rows
--- rowCommitter :: CsvConfig -> Cont IO (Committer IO [Text])
+-- | commits printed csv rows
+--
+-- >>> let testConfig = CsvConfig "test" ".csv" "./test" ',' NoHeader
+-- >>> let ctest = rowCommitter testConfig (fmap (Text.intercalate "," . fmap show))
+-- >>> ctest `with` (\c -> commit c [[1..10::Int]])
+-- True
+--
+-- >>> rowEmitter testConfig ints `with` emit
+-- Just (Right [1,2,3,4,5,6,7,8,9,10])
+--
 rowCommitter :: CsvConfig -> (a -> [Text]) -> Cont IO (Committer IO a)
-rowCommitter cfg f = contramap (Text.intercalate (Text.singleton $ view #csep cfg) . f) <$> fileCommitter (file cfg)
+rowCommitter cfg f = contramap (Text.intercalate (Text.singleton $ view #fsep cfg) . f) <$> fileCommitter (file cfg)
 
 -- | Run a parser across all lines of a file.
 --
--- >>> xss <- rights <$> runCsv defaultCsvConfig doubles
+-- >>> r1 <- runCsv defaultCsvConfig fields
+-- >>> length r1
+-- 42562
 --
--- >>> length xss
--- 1212
+-- >>> length [x | (Left x) <- r1]
+-- 0
 --
--- >>> all ((==101) . length) xss
--- True
---
--- >>> take 2 xss
--- [[39.02,36.49,38.2,38.85,39.38,39.74,37.02,39.53,38.81,38.79,37.65,39.34,38.55,39.03,37.21,36.32,37.81,38.95,36.7,39.72,37.06,37.29,36.43,36.53,36.19,38.17,37.3,36.15,36.68,36.7,36.68,36.99,38.92,37.25,37.47,36.32,35.75,35.68,34.66,34.26,35.62,36.6,34.78,34.67,34.3,33.4,31.4,31.75,31.75,32.84,33.76,35.74,34.01,33.91,36.88,34.41,35.52,36.94,36.95,35.57,38.02,37.32,39.05,37.97,37.01,38.98,38.83,38.87,38.03,38.4,38.25,38.61,36.23,37.81,37.98,38.58,38.96,38.97,39.08,38.79,38.79,36.31,36.59,38.19,37.95,39.63,39.27,37.19,37.13,37.47,37.57,36.62,36.92,38.8,38.52,38.07,36.73,39.46,37.5,39.1,0.0],[1.83,1.71,1.77,1.77,1.68,1.78,1.8,1.7,1.75,1.78,1.86,1.76,1.81,1.86,1.74,1.78,1.81,2.02,2.0,2.01,2.0,2.06,2.0,1.93,1.88,1.85,1.89,1.83,1.76,1.83,1.81,1.81,1.78,1.85,1.86,1.73,1.79,1.81,1.85,1.71,1.71,1.71,1.84,1.76,1.73,1.83,1.68,1.73,1.76,1.77,1.72,1.75,1.66,1.76,1.77,1.78,1.63,1.72,1.66,1.67,1.74,1.65,1.74,1.79,1.69,1.76,1.74,1.82,1.78,1.65,1.65,1.82,1.71,1.83,1.72,1.63,1.77,1.69,1.81,1.74,1.7,1.72,1.74,1.72,1.74,1.71,1.7,1.83,1.79,1.78,1.71,1.8,1.79,1.77,1.74,1.74,1.8,1.78,1.75,1.69,1.0]]
+-- >>> take 2 $ drop 2 [x | (Right x) <- r1]
+-- [["","Afghanistan","33.0","65.0","2020-06-29","733","AFG","142","34","\r"],["","Afghanistan","33.0","65.0","2020-06-28","721","AFG","142","34","\r"]]
 runCsv :: CsvConfig -> (Char -> A.Parser a) -> IO [Either Text a]
-runCsv cfg p = with (rowEmitter cfg p) toList'
-
--- Convert an emitter to a list.
-toList' :: Emitter IO a -> IO [a]
-toList' e = go []
-  where
-    go xs = do
-      r <- emit e
-      case r of
-        Nothing -> pure (reverse xs)
-        Just x -> go (x : xs)
+runCsv cfg p = toListE (rowEmitter cfg p)
 
 -- * low-level generic csv parser helpers
 
--- $setup
--- >>> :set -XOverloadedStrings
-
--- | Most parsing and building routines implicity assume comma separated, and newlines separating rows.
+-- | Most parsing and building routines implicity assume a character acting as a separator of fields, and newlines separating rows.
 --
 -- >>> A.parse (sep ',') ",ok"
 -- Done "ok" ()
 sep :: Char -> A.Parser ()
 sep c = void (A.char c)
 
+-- * single field parsers
+
 -- | an unquoted field
 -- Does not consume the separator token
--- >>> A.parse (field ',') "field,ok"
+--
+-- >>> A.parse (field_ ',') "field,ok"
 -- Done ",ok" "field"
+field_ :: Char -> A.Parser Text
+field_ c = A.takeWhile (/= c)
+
+-- | an unquoted field
+-- Consume the separator token
+--
+-- >>> A.parse (field ',') "field,ok"
+-- Done "ok" "field"
 field :: Char -> A.Parser Text
-field c = A.takeWhile (/= c)
+field c = A.takeWhile (/= c) <* A.char c
 
 -- | skipping a field
--- >>> A.parse (skipField ',') "field,ok"
+--
+-- >>> A.parse (skipField_ ',') "field,ok"
 -- Done ",ok" ()
+skipField_ :: Char -> A.Parser ()
+skipField_ c = A.skipWhile (/= c)
+
+-- | skipping a field
+--
+-- >>> A.parse (skipField ',') "field,ok"
+-- Done "ok" ()
 skipField :: Char -> A.Parser ()
-skipField c = A.skipWhile (/= c)
+skipField c = A.skipWhile (/= c) <* A.char ','
+
+-- | int parser
+--
+-- >>> A.parse int "234,ok"
+-- Done ",ok" 234
+int :: A.Parser Int
+int = A.decimal
+
+-- | int parser, consumes separator
+--
+-- >>> A.parse (int' ',') "234,ok"
+-- Done "ok" 234
+int' :: Char -> A.Parser Int
+int' c = A.decimal <* A.char c
+
+-- | double parser, consumes separator
+--
+-- >>> A.parse (double' ',') "234.000,ok"
+-- Done "ok" 234.0
+double' :: Char -> A.Parser Double
+double' c = A.double <* A.char c
 
 -- * Block list parsers
-
--- | parser for a csv row of [Text]
--- >>> A.parseOnly (texts ',') "field1,field2"
--- Right ["field1","field2"]
-texts :: Char -> A.Parser [Text]
-texts c = field c `A.sepBy1` sep c
+-- | Parser for a csv row of [Text].
+-- TODO: deal with potential for an extra '\r'
+--
+-- >>> A.parseOnly (fields ',') "field1,field2\r"
+-- Right ["field1","field2\r"]
+--
+fields :: Char -> A.Parser [Text]
+fields c =
+  field_ c `A.sepBy1` (sep c)
 
 -- | parser for a csv row of [Scientific]
+--
 -- >>> A.parseOnly (scis ',') "1,2.2,3.3"
 -- Right [1.0,2.2,3.3]
 scis :: Char -> A.Parser [Scientific]
 scis c = A.scientific `A.sepBy1` sep c
 
 -- | parser for a csv row of [Double]
+--
 -- >>> A.parseOnly (doubles ',') "1,2,3"
 -- Right [1.0,2.0,3.0]
 doubles :: Char -> A.Parser [Double]
 doubles c = A.double `A.sepBy1` sep c
 
 -- | parser for a csv row of [Int]
+--
 -- >>> A.parseOnly (ints ',') "1,2,3"
 -- Right [1,2,3]
 ints :: Char -> A.Parser [Int]
